a6e002222b76dca59aa18a99eb83b5f5e680b2e9
Ayoub <ayoub.ghrs@gmail.com>
2018-09-26 20:00:49 +0900

    OK

--- networks/networks.py
+++ networks/networks.py
@@ -23,7 +23,7 @@ class FCSpectralRNet(BaseN.BaseNetwork):
         
         x = input_shape
         self.model = nn.Sequential(BaseN.Flatten(),
-                                   nn.Linear(np.prod(x), 1024),nn.ReLU(),
+                                   nn.Linear(np.prod(x), 1024),nn.Softplus(),
                                    nn.Linear(1024,1024),nn.Tanh(),
                                    nn.Linear(1024,512),BaseN.AdaptiveTanh(),
                                    nn.Linear(512,self.output_shape[0]-1),
